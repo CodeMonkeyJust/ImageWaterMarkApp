cc09d48a593186e3b8c92906a5265b87f7745987
CodeMonkey <zlj20@163.com>
2023-04-09 20:11:49 +0800

    添加主界面

--- src/mainform.py
+++ src/mainform.py
@@ -1,141 +1,269 @@
-# encoding= utf-8
-# ------------------------------------------------------------------
-# Author:zhulijun
-# Created:2021-09-09
-# Description:邮件发送主GUI窗体
-# ------------------------------------------------------------------
-from tkinter import *
-import hashlib
-import time
-from tkinter.filedialog import (askopenfilename,
-                                askopenfilenames,
-                                askdirectory,
-                                asksaveasfilename)
-from configunit import get_config
-from dateunit import get_curdate_last_month
-import tkinter.messagebox
-import os
-from workspace import get_config_filename, init_file
-
-LOG_LINE_NUM = 0
-EXCEL_FILE_PATH = ''
+import tkinter as tk
+import tkinter.font as tkFont
 
 
-class MY_GUI():
-    def __init__(self, init_window_name):
-        self.init_window_name = init_window_name
+class App:
+    def __init__(self, root):
+        # setting title
+        root.title("图像水印工具 For Flyany V1.0.0")
+        # setting window size
+        width = 700
+        height = 702
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.geometry(alignstr)
+        root.resizable(width=False, height=False)
 
-    # 设置窗口
-    def set_init_window(self):
-        self.init_window_name.title("图像水印工具 For Flyany V1.0.0")  # 窗口名
-        # self.init_window_name.geometry('320x160+100+100')                         #290 160为窗口大小，+10 +10 定义窗口弹出时的默认展示位置
-        self.init_window_name.geometry('800x600+10+10')
-        # self.init_window_name["bg"] = "pink"                                    #窗口背景色，其他背景色见：blog.csdn.net/chl0000/article/details/7657887
-        # self.init_window_name.attributes("-alpha",0.9)                          #虚化，值越小虚化程度越高
-        # 阻止Python GUI的大小调整
-        self.init_window_name.resizable(0, 0)
-        # 窗口图标
-        # self.init_window_name.iconbitmap(r".\app.ico")
-        # 标签
-        self.execl_label = Label(self.init_window_name, text="工资表")
-        self.execl_label.grid(row=1, column=0)
-        self.excel_entry = Entry(self.init_window_name, width=80)
-        self.excel_entry.grid(row=1, column=1)
+        GButton_79 = tk.Button(root)
+        GButton_79["bg"] = "#f0f0f0"
+        ft = tkFont.Font(family='Times', size=10)
+        GButton_79["font"] = ft
+        GButton_79["fg"] = "#000000"
+        GButton_79["justify"] = "center"
+        GButton_79["text"] = "..."
+        GButton_79.place(x=560, y=20, width=84, height=30)
+        GButton_79["command"] = self.GButton_79_command
 
-        self.init_sender_label = Label(self.init_window_name, text="发件人")
-        self.init_sender_label.grid(row=2, column=0)
-        self.mailsender_label = Label(self.init_window_name,
-                                      text=get_config(get_config_filename(), 'send', 'mail_sender'))
-        self.mailsender_label.grid(row=2, column=1)
+        GLabel_855 = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GLabel_855["font"] = ft
+        GLabel_855["fg"] = "#333333"
+        GLabel_855["justify"] = "right"
+        GLabel_855["text"] = "水印文件目录："
+        GLabel_855.place(x=20, y=20, width=90, height=30)
 
-        self.init_yearmonth_label = Label(self.init_window_name, text="邮件标题")
-        self.init_yearmonth_label.grid(row=3, column=0)
-        self.init_data_label = Label(self.init_window_name, text="签名信息信息一")
-        self.init_data_label.grid(row=4, column=0)
-        self.init_data_label = Label(self.init_window_name, text="签名信息信息二")
-        self.init_data_label.grid(row=5, column=0)
-        self.init_data_label = Label(self.init_window_name, text="工资数据")
-        self.init_data_label.grid(row=6, column=0)
-        self.log_label = Label(self.init_window_name, text="发送结果")
-        self.log_label.grid(row=13, column=0)
-        # 文本框
-        self.data_text = Text(self.init_window_name, width=100, height=18)  # 原始数据录入框
-        self.data_text.grid(row=7, column=0, rowspan=5, columnspan=10)
-        self.log_data_text = Text(self.init_window_name, width=100, height=18)  # 日志框
-        self.log_data_text.grid(row=14, column=0, columnspan=5)
+        GLabel_669 = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GLabel_669["font"] = ft
+        GLabel_669["fg"] = "#333333"
+        GLabel_669["justify"] = "right"
+        GLabel_669["text"] = "水印位置："
+        GLabel_669.place(x=20, y=280, width=90, height=30)
 
-        # 输入框
-        self.mailsubject_entry = Entry(self.init_window_name, width=80)
-        self.mailsubject_entry.grid(row=3, column=1)
-        self.mailsubject_entry.insert(0, get_curdate_last_month() + "工资条-")
+        GLabel_72 = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GLabel_72["font"] = ft
+        GLabel_72["fg"] = "#333333"
+        GLabel_72["justify"] = "right"
+        GLabel_72["text"] = "图像/水印大小："
+        GLabel_72.place(x=20, y=460, width=90, height=30)
 
-        self.mailsign1_entry = Entry(self.init_window_name, width=80)
-        self.mailsign1_entry.grid(row=4, column=1)
-        self.mailsign1_entry.insert(0, get_config(get_config_filename(), 'send', 'sign1'))
+        GLineEdit_WatermarkDir = tk.Entry(root)
+        GLineEdit_WatermarkDir["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times', size=10)
+        GLineEdit_WatermarkDir["font"] = ft
+        GLineEdit_WatermarkDir["fg"] = "#333333"
+        GLineEdit_WatermarkDir["justify"] = "center"
+        GLineEdit_WatermarkDir["text"] = ""
+        GLineEdit_WatermarkDir.place(x=130, y=20, width=400, height=30)
 
-        self.mailsign2_entry = Entry(self.init_window_name, width=80)
-        self.mailsign2_entry.grid(row=5, column=1)
-        self.mailsign2_entry.insert(0, get_config(get_config_filename(), 'send', 'sign2'))
-        # 按钮
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="选择工资表", width=10,
-                                              command=self.choose_excel)  # 调用内部方法  加()为直接调用
-        self.str_trans_to_md5_button.grid(row=0, column=0)
+        GListBox_903 = tk.Listbox(root)
+        GListBox_903["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times', size=10)
+        GListBox_903["font"] = ft
+        GListBox_903["fg"] = "#333333"
+        GListBox_903["justify"] = "center"
+        GListBox_903.place(x=130, y=80, width=512, height=170)
 
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="发送工资条", width=10,
-                                              command=self.send_salary_mail)  # 调用内部方法  加()为直接调用
-        self.str_trans_to_md5_button.grid(row=0, column=1)
+        GLabel_268 = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GLabel_268["font"] = ft
+        GLabel_268["fg"] = "#333333"
+        GLabel_268["justify"] = "right"
+        GLabel_268["text"] = "透明度："
+        GLabel_268.place(x=380, y=460, width=90, height=30)
 
-    def choose_excel(self):
-        global EXCEL_FILE_PATH
-        EXCEL_FILE_PATH = askopenfilename(title='选择工资表 EXCEL', filetypes=[('XLSX', '*.xlsx'), ('XLS', '*.xls')])
-        self.excel_entry.delete(0, END)
-        self.excel_entry.insert(0, EXCEL_FILE_PATH)
-        # 清空
-        self.data_text.delete(1.0, END)
-        if os.path.exists(EXCEL_FILE_PATH):
-            data = load_user_mail(EXCEL_FILE_PATH)
-            for j in range(len(data[0])):
-                # print(data[0][j])
-                # char(122288)为中文空格，修正Format格式化中文补位问题
-                self.data_text.insert(END,
-                                      '{0:{2}<10}|\t{1:{2}<10}\n'.format(data[0][j], data[1][j], chr(122288), end=''))
+        GLineEdit_518 = tk.Entry(root)
+        GLineEdit_518["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times', size=10)
+        GLineEdit_518["font"] = ft
+        GLineEdit_518["fg"] = "#333333"
+        GLineEdit_518["justify"] = "center"
+        GLineEdit_518["text"] = "3"
+        GLineEdit_518.place(x=120, y=460, width=150, height=30)
 
-    def send_salary_mail(self):
-        if os.path.exists(EXCEL_FILE_PATH):
-            self.write_log_to_text('开始发送工资条')
-            send_by_excel(EXCEL_FILE_PATH, self.mailsubject_entry.get(), self.mailsign1_entry.get(),
-                          self.mailsign2_entry.get())
-            self.write_log_to_text('工资条发送完成')
-        else:
-            tkinter.messagebox.showerror(title='文件错误', message='请选择工资表EXCLE文件，最后一列必须为电子邮件地址！')
+        GLabel_54 = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GLabel_54["font"] = ft
+        GLabel_54["fg"] = "#333333"
+        GLabel_54["justify"] = "right"
+        GLabel_54["text"] = "图像文件："
+        GLabel_54.place(x=20, y=520, width=90, height=30)
 
-    # 获取当前时间
-    def get_current_time(self):
-        current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        return current_time
+        GLineEdit_962 = tk.Entry(root)
+        GLineEdit_962["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times', size=10)
+        GLineEdit_962["font"] = ft
+        GLineEdit_962["fg"] = "#333333"
+        GLineEdit_962["justify"] = "center"
+        GLineEdit_962["text"] = ""
+        GLineEdit_962.place(x=120, y=520, width=400, height=30)
 
-    # 日志动态打印
-    def write_log_to_text(self, logmsg):
-        global LOG_LINE_NUM
-        current_time = self.get_current_time()
-        logmsg_in = str(current_time) + " " + str(logmsg) + "\n"  # 换行
-        if LOG_LINE_NUM <= 7:
-            self.log_data_text.insert(END, logmsg_in)
-            LOG_LINE_NUM = LOG_LINE_NUM + 1
-        else:
-            self.log_data_text.delete(1.0, 2.0)
-            self.log_data_text.insert(END, logmsg_in)
+        GButton_389 = tk.Button(root)
+        GButton_389["bg"] = "#f0f0f0"
+        ft = tkFont.Font(family='Times', size=10)
+        GButton_389["font"] = ft
+        GButton_389["fg"] = "#000000"
+        GButton_389["justify"] = "center"
+        GButton_389["text"] = "..."
+        GButton_389.place(x=540, y=520, width=84, height=30)
+        GButton_389["command"] = self.GButton_389_command
+
+        GButton_522 = tk.Button(root)
+        GButton_522["bg"] = "#f0f0f0"
+        ft = tkFont.Font(family='Times', size=10)
+        GButton_522["font"] = ft
+        GButton_522["fg"] = "#000000"
+        GButton_522["justify"] = "center"
+        GButton_522["text"] = "生成水印图片"
+        GButton_522.place(x=260, y=590, width=165, height=30)
+        GButton_522["command"] = self.GButton_522_command
+
+        GRadio_385 = tk.Radiobutton(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GRadio_385["font"] = ft
+        GRadio_385["fg"] = "#333333"
+        GRadio_385["justify"] = "left"
+        GRadio_385["text"] = "左上"
+        GRadio_385.place(x=130, y=280, width=90, height=30)
+        GRadio_385["command"] = self.GRadio_385_command
+
+        GRadio_873 = tk.Radiobutton(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GRadio_873["font"] = ft
+        GRadio_873["fg"] = "#333333"
+        GRadio_873["justify"] = "right"
+        GRadio_873["text"] = "右上"
+        GRadio_873.place(x=550, y=280, width=90, height=30)
+        GRadio_873["command"] = self.GRadio_873_command
+
+        GLabel_190 = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GLabel_190["font"] = ft
+        GLabel_190["fg"] = "#333333"
+        GLabel_190["justify"] = "right"
+        GLabel_190["text"] = "请选择水印："
+        GLabel_190.place(x=20, y=80, width=90, height=30)
+
+        GLineEdit_887 = tk.Entry(root)
+        GLineEdit_887["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times', size=10)
+        GLineEdit_887["font"] = ft
+        GLineEdit_887["fg"] = "#333333"
+        GLineEdit_887["justify"] = "center"
+        GLineEdit_887["text"] = "0"
+        GLineEdit_887.place(x=490, y=460, width=150, height=30)
+
+        GRadio_894 = tk.Radiobutton(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GRadio_894["font"] = ft
+        GRadio_894["fg"] = "#333333"
+        GRadio_894["justify"] = "left"
+        GRadio_894["text"] = "左中"
+        GRadio_894.place(x=130, y=340, width=90, height=30)
+        GRadio_894["command"] = self.GRadio_894_command
+
+        GRadio_92 = tk.Radiobutton(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GRadio_92["font"] = ft
+        GRadio_92["fg"] = "#333333"
+        GRadio_92["justify"] = "center"
+        GRadio_92["text"] = "中上"
+        GRadio_92.place(x=340, y=280, width=90, height=30)
+        GRadio_92["command"] = self.GRadio_92_command
+
+        GRadio_330 = tk.Radiobutton(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GRadio_330["font"] = ft
+        GRadio_330["fg"] = "#333333"
+        GRadio_330["justify"] = "center"
+        GRadio_330["text"] = "中中"
+        GRadio_330.place(x=340, y=340, width=90, height=30)
+        GRadio_330["command"] = self.GRadio_330_command
+
+        GRadio_923 = tk.Radiobutton(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GRadio_923["font"] = ft
+        GRadio_923["fg"] = "#333333"
+        GRadio_923["justify"] = "right"
+        GRadio_923["text"] = "右中"
+        GRadio_923.place(x=550, y=340, width=90, height=30)
+        GRadio_923["command"] = self.GRadio_923_command
+
+        GRadio_31 = tk.Radiobutton(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GRadio_31["font"] = ft
+        GRadio_31["fg"] = "#333333"
+        GRadio_31["justify"] = "left"
+        GRadio_31["text"] = "左下"
+        GRadio_31.place(x=130, y=400, width=90, height=25)
+        GRadio_31["command"] = self.GRadio_31_command
+
+        GRadio_639 = tk.Radiobutton(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GRadio_639["font"] = ft
+        GRadio_639["fg"] = "#333333"
+        GRadio_639["justify"] = "center"
+        GRadio_639["text"] = "中下"
+        GRadio_639.place(x=340, y=400, width=90, height=30)
+        GRadio_639["command"] = self.GRadio_639_command
+
+        GRadio_95 = tk.Radiobutton(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GRadio_95["font"] = ft
+        GRadio_95["fg"] = "#333333"
+        GRadio_95["justify"] = "right"
+        GRadio_95["text"] = "右下"
+        GRadio_95.place(x=550, y=400, width=90, height=30)
+        GRadio_95["command"] = self.GRadio_95_command
+
+        GLabel_630 = tk.Label(root)
+        ft = tkFont.Font(family='Times', size=10)
+        GLabel_630["font"] = ft
+        GLabel_630["fg"] = "#333333"
+        GLabel_630["justify"] = "center"
+        GLabel_630["text"] = "/1"
+        GLabel_630.place(x=280, y=460, width=35, height=30)
+
+    def GButton_79_command(self):
+        print("command")
+
+    def GButton_389_command(self):
+        print("command")
+
+    def GButton_522_command(self):
+        print("command")
+
+    def GRadio_385_command(self):
+        print("command")
+
+    def GRadio_873_command(self):
+        print("command")
+
+    def GRadio_894_command(self):
+        print("command")
+
+    def GRadio_92_command(self):
+        print("command")
+
+    def GRadio_330_command(self):
+        print("command")
+
+    def GRadio_923_command(self):
+        print("command")
+
+    def GRadio_31_command(self):
+        print("command")
+
+    def GRadio_639_command(self):
+        print("command")
+
+    def GRadio_95_command(self):
+        print("command")
 
 
-def gui_start():
-    init_file()
-    init_window = Tk()  # 实例化出一个父窗口
-    ZMJ_PORTAL = MY_GUI(init_window)
-    # 设置根窗口默认属性
-    ZMJ_PORTAL.set_init_window()
-
-    init_window.mainloop()  # 父窗口进入事件循环，可以理解为保持窗口运行，否则界面不展示
-
-
-if __name__ == '__main__':
-    gui_start()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
